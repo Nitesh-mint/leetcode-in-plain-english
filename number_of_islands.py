50783856349323eaf2dd85ff8dff1de726495313
Nitesh-mint <niteshraya058@protonmail.com>
2026-05-04 08:32:22 +0545

    added another problem

--- number_of_islands.py
+++ number_of_islands.py
@@ -29,6 +29,25 @@ Output: 4"""
 from typing import List
 
 
+# DFS Solution
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        return 0
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for c in range(COLS):
+            for r in range(ROWS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+        return islands
